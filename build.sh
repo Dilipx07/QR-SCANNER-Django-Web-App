efb85f8f19cadc7ffe57c16ec2d50911dab4ee9b
Dilip <149667659+Dilipx07@users.noreply.github.com>
2026-07-01 09:44:20 +0530

    Update manage.py command calls to use -m flag

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate --no-input
-python manage.py seed_scanner_login
+python -m manage.py collectstatic --no-input
+python -m manage.py migrate --no-input
+python -m manage.py seed_scanner_login
